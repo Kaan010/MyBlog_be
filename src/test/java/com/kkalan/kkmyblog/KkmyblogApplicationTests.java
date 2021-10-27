72997cdca41b2f80ea1d78090d25ada77f801990
Kaan Kalan <68256356+Kaan010@users.noreply.github.com>
2021-10-28 01:16:51 +0300

    Initial commit

---
+++ src/test/java/com/kkalan/kkmyblog/KkmyblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kkalan.kkmyblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KkmyblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
